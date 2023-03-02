12859640076e9cf91042a0e57187cbb7080c7856
HotBaconWasTaken <116873925+nicola-stroffolino-scuola@users.noreply.github.com>
2023-03-02 12:21:13 +0100

    Default Project

---
+++ Giardino/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Giardino;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Giardino/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Giardino;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
